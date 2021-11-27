77834bf5b3ca98333b02dd47c0351f682ac59ac2
Gabriel-AlvesP <190221057@estudantes.ips.pt>
2021-11-27 12:36:50 +0000

    Base files

---
+++ procura.lisp
@@ -0,0 +1,6 @@
+;   Deve conter a implementacao de:
+
+;;  Algoritmo de procura de Largura Primeiro (BFS)
+;;  Algoritmo de Procura do Profundidade Primeiro (DFS)
+;;  Algoritmo de Procura do Melhor Primeiro (A*)
+;;  Os algoritmos SMA*, IDA* e/ou RBFS (bonus)
---
+++ projeto.lisp
@@ -0,0 +1,5 @@
+; Carrega os outros ficheiros de codigo,
+; Escreve e le ficheiros, 
+; Trata da interacao com o utilizador
+
+
