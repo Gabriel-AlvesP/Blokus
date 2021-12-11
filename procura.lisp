cd1beadd549c2f9584704db24cf7387d71a24439
Gabriel-AlvesP <190221057@estudantes.ips.pt>
2021-12-11 21:31:36 +0000

    bfs

--- procura.lisp
+++ procura.lisp
@@ -3,6 +3,7 @@
 ;;;; Gabriel Pais - 201900301
 ;;;; Andre Serrado - 201900318
 
+;(load "projeto.lisp")
 
 ;;; Abstract Data Types
 
@@ -26,21 +27,20 @@
   (third node)
 )
 
-(defun new-child(node operation &aux (state (node-state node)))
+(defun get-child(node operation &aux (state (node-state node)))
 	(cond 
 		((null (funcall operation state)) nil)
 		(t (list (funcall operation state) (1+ (depth node)) node)) 
 	)
 )
 
-(defun all-children(node operations-list alg &optional d)
+(defun get-children(node operations-list alg &optional d)
 	(cond
 		((null operations-list) nil)
 		((and (equal alg 'dfs) (< d (1+ (depth node)))) nil)
-		(t (cons (new-child node (car operations-list)) (all-children node (cdr operations-list) alg d)))
+		(t (cons (get-child node (car operations-list)) (get-children node (cdr operations-list) alg d)))
 	)
 )
-
 
 (defun exist-nodep(node node-list)
   (cond 
@@ -51,7 +51,32 @@
 
 ;;;  Algoritmo de procura de Largura Primeiro (BFS)
 
+;; 
+;  returns a list with all non expanded children
+(defun remove-duplicated(children open closed) 
+        (cond
+            ((or(null children) (null open) (null closed)) nil)
+            ((eval (cons 'or (mapcar 'exist-nodep  (car children) open))) (remove-duplicated (cdr children) open closed))
+            ((eval (cons 'or (mapcar 'exist-nodep  (car children) closed))) (remove-duplicated (cdr children) open closed))
+            (t (cons (car children) (remove-duplicated (cdr children) open closed)))
+        )
+)
 
+;; bfs
+;  returns a found node solution
+(defun bfs(solution-fun get-children operations &optional (closed nil) (opened nil)  node-init)
+    (cond 
+        ((not (null node-init))  (bfs solution-fun get-children operations closed (cons node-init opened)))
+        ((null opened) nil)
+        (t (let ((current-node (car opened)) (children (remove-duplicated (get-children node-init operations 'bfs) opened closed))) 
+                (cond 
+                    ((mapcar 'solution-fun (append (cdr opened) children)) current-node)
+                    (t (bfs solution-fun get-children operations (cons (car opened) closed) (append (cdr opened) children)))
+                )
+            )
+        )  
+    )
+)
 
 ;;;  Algoritmo de Procura do Profundidade Primeiro (DFS)
 
