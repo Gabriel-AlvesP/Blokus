6f4304f5278882416bafbb49b2283fc1b973cc0b
André Serrado <58612578+AndreSerrado@users.noreply.github.com>
2021-12-12 02:21:40 +0000

    Update projeto.lisp

--- projeto.lisp
+++ projeto.lisp
@@ -9,24 +9,74 @@
 ;; init-menu 
 ;  shows the game's initial menu
 (defun init-menu()
-"Mostra o menu inicial"
   (progn
-  	(format t "~%___________________________________~%~%>")
-    (format t "~%\            BLOKUS               / ")
-    (format t "~%/                                 \ ")
-    (format t "~%\  1 - Resolver um tabuleiro      /")
-    (format t "~%/  2 - Mostrar um tabuleiro       \ ")
-    (format t "~%\  3 - Sair                       /")
-    (format t "~%/_________________________________\ ~%~%>")
+    (format t "~%_________________________________________________________")
+    (format t "~%\\                      BLOKUS UNO                       /")
+    (format t "~%/                                                       \\")
+    (format t "~%\\     1 - Resolver um tabuleiro                         /")
+    (format t "~%/     2 - Mostrar um tabuleiro                          \\")
+    (format t "~%\\     3 - Sair                                          /")
+    (format t "~%/_______________________________________________________\\~%~%>")
   )
 )
 
+(defun choose-algorithm()
+  (progn
+    (format t "~%_________________________________________________________")
+    (format t "~%\\                      BLOKUS UNO                       /")
+    (format t "~%/                  Escolha o algoritmo                  \\")
+    (format t "~%\\                                                       /")
+    (format t "~%/     1 - Breadth-First Search                          \\")
+    (format t "~%\\     2 - Depth-First Search                            /")
+    (format t "~%/     3 - A*                                            \\")
+    (format t "~%\\     4 - IDA*                                          /")
+    (format t "~%/     0 - Voltar                                        \\")
+    (format t "~%\\                                                       /")
+    (format t "~%/_______________________________________________________\\~%~%> ")
+  )
+)
+
+(defun choose-heuristic()
+  (format t "~%_________________________________________________________")
+  (format t "~%\\                      BLOKUS UNO                       /")
+  (format t "~%/                 Escolha uma heurística                \\")
+  (format t "~%\\                                                       /")
+  (format t "~%/     1 - Heurística Base                               \\")
+  (format t "~%\\     2 - Heurística Desenvolvida                       /")
+  (format t "~%/     0 - Voltar                                        \\")
+  (format t "~%\\                                                       /")
+  (format t "~%/_______________________________________________________\\~%~%> ")
+)
+
+(defun choose-depth()
+  (format t "~%__________________________________________________________")
+  (format t "~%\\                      BLOKUS UNO                        /")
+  (format t "~%/           Insira a profundidade máxima desejada        \\")
+  (format t "~%\\                     -1 - Voltar                        /")
+  (format t "~%/________________________________________________________\\~%~%> ")
+)
 
 ;;; Files Handler
 
 
 ;;; User Handler
-
+;; start
+;  Receive data from the keyboard, by the user in relation to the option he want to choose
+(defun start()
+  (progn
+    (init-menu)
+    (let ((option (read)))
+      (if (or (not (numberp option)) (< option 1) (> option 3))
+          (progn (format t "Opção inválida") (start))
+        (case option
+          ('1 (format t "Opção 1"))
+          ('2 (format t "Opção 2"))
+          ('3 (format t "Adeus!"))
+          )
+        )
+      )
+    )
+)
 
 
 ;;;Stats
