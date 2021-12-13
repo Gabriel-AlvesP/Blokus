12f2e963d58c38c28e70eb62064f430553014cfd
André Serrado <58612578+AndreSerrado@users.noreply.github.com>
2021-12-13 02:58:03 +0000

    Update projeto.lisp

--- projeto.lisp
+++ projeto.lisp
@@ -58,6 +58,15 @@
 
 ;;; Files Handler
 
+;; get-problems-path
+;  returns the path to the problems.dat file
+;(defun get-problems-path ())
+
+;; get-solutions-path
+;  returns the path to the file
+;(defun get-problems-path ())
+
+
 
 ;;; User Handler
 ;; start
@@ -78,9 +87,82 @@
     )
 )
 
+;; read-depth-chosen
+;  read the depth chosen by the user
+(defun read-depth-chosen ()
+  (if (not (choose-depth))
+      (let ((option (read)))
+         (cond ((eq option '-1) );(exec-search))
+               ((or (not (numberp option)) (< option -1)) 
+                (progn (format t "Opção inválida")) (read-depth-chosen))
+               (t option))))
+)
 
+;; read-heuristic-chosen
+;  read the heuristic chosen by the user
+(defun read-heuritic-chosen ()
+  (if (not (choose-heuristic))
+      (let ((option (read)))
+         (cond ((eq option '0) );(exec-search))
+               ((or (not (numberp option)) (< option 0)) 
+                (progn (format t "Opção inválida")) (read-heuritic-chosen))
+               ;((eq option 1) 'base-heuristic)
+               ;(T 'best-heuristic)
+     )))
+)
+)
 ;;;Stats
 
+;; write-bfs-dfs-stats
+;  write the solution and measures for the bfs and dfs algorithms
+#|
+(defun write-bfs-dfs-stats (search)
+  (progn
+    ;(format "~% *** Solução do Tabuleiro: ***")
+    (format "")
+    (format "~%~t> Algoritmo:")
+    (format "~%~t> Início:")
+    (format "~%~t> Fim:")
+    (format "~%~t> Tempo Total:")
+    (format "~%~t> Fator de ramificação média:")
+    (format "~%~t> Número de nós gerados:")
+    (format "~%~t> Número de nós expandidos:")
+    (format "~%~t> Penetrância:")
+    (if (eq search 'dfs)
+        (format "~%~t> Profundidade máxima:"))
+    (format "~%~t> Comprimento da solução:")
+    (format "~%~t> Estado Inicial:")
+    ;função para mostrar o estado inical do tabuleiro
+    (format "~%~t> Estado Final:")
+    ;função para a solução do tabuleiro
+   )
+)
+|#
+
+
+;; write-a*-stats
+;  write the solution and measures for the a* algorithm
+#|
+(defun write-a*-stats (search)
+  (progn
+    ;(format "~% *** Solução do Tabuleiro: ***")
+    (format "")
+    (format "~%~t> Algoritmo:")
+    (format "~%~t> Início:")
+    (format "~%~t> Fim:")
+    (format "~%~t> Tempo Total:")
+    (format "~%~t> Fator de ramificação média:")
+    (format "~%~t> Número de nós gerados:")
+    (format "~%~t> Número de nós expandidos:")
+    (format "~%~t> Penetrância:")
+    (format "~%~t> Comprimento da solução:")
+    (format "~%~t> Estado Inicial:")
+    ;função para mostrar o estado inical do tabuleiro
+    (format "~%~t> Estado Final:")
+    ;função para a solução do tabuleiro
+   )
+)
+|#
 ;; current-time
 ;  returns a list with a actual time (hours minutes seconds)
 (defun current-time()
